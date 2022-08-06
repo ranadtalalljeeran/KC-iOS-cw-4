b1cb0251023d996347f327fb462fed47459eb66c
ranadtalalljeeran <ranadtalalaljiran@gmail.com>
2022-08-06 14:52:16 +0300

    done

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+var birthYear = 2007
+var name = "Ranad"
+var age : Int
+var currentYear = 2022
+//Hint: current year - birthYear
+age = currentYear - birthYear
+
+//Hello my name is your name and my age is your age
+
+print("Hello my name is \(name) and my age is \(age)")
+if age >= 14 || age <= 18
+{
+   print("نعم يمكنك المشاركة في الكويت تبرمج")
+}
+ else
+{
+     print("لايمكنك المشاركة في الكويت تبرمج")
+ }
